7cb44a0bb9fd16d93914f4860a263e099753b03f
jaycopeland <jay_copeland@hms.harvard.edu>
2017-07-17 14:09:16 -0400

    deleted unneeded files and added script that worked.  Please enter the commit message for your changes. Lines starting

--- stitcher_0.4.0_testing/submit_stitch.sbatch.sh
+++
@@ -1,9 +0,0 @@
-# loop over all the directories containing rcpnl files
-# print the directory name (so we have some visual progress indicator)
-# then submit the stitcher jobs to SLURM
-#
-for DIR in *; do
-echo ${DIR} # print the directory name
-sbatch -o ${DIR}.stdout.txt -e ${DIR}.stderr.txt stitch_many.sbatch ${DIR}
-sleep 1 # pause to be kind to the scheduler
-done
